d78c9125d2a0e2b4b5785b7451d631dc1b533cb4
jjunsik <junsik8236@gmail.com>
2024-03-22 16:16:12 +0900

    [CD] CodeDeploy가 참조할 파일과 배포 파일 생성

---
+++ scripts/deploy.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+# 이 배포 스크립트는 Spring Boot 애플리케이션을 배포하기 위해 사용되며
+# 이전에 실행중인 프로세스를 중단하고 최신 버전의 JAR 파일을 실행하는 작업을 수행한다.
+[CD]
+REPOSITORY=/home/ubuntu/wakuwaku
+cd $REPOSITORY
+
+APP_NAME=wakuwaku
+JAR_NAME=$(ls $REPOSITORY/build/libs/ | grep 'SNAPSHOT.jar' | tail -n 1)
+JAR_PATH=$REPOSITORY/build/libs/$JAR_NAME
+
+CURRENT_PID=$(pgrep -f $APP_NAME)
+
+if [ -z $CURRENT_PID ]
+then
+  echo "> 종료할 애플리케이션이 없습니다."
+else
+  echo "> kill -9 $CURRENT_PID"
+  kill -15 $CURRENT_PID
+  sleep 5
+fi
+
+echo "> Deploy - $JAR_PATH "
+nohup java -jar $JAR_PATH > /dev/null 2> /dev/null < /dev/null &
