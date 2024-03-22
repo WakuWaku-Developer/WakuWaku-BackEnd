77cf0ff0c8ff05e8218bdb8b1b53be3579740fd4
jjunsik <junsik8236@gmail.com>
2024-03-22 15:30:24 +0900

    Init Commit

---
+++ src/main/java/dev/backend/WakuWaku/WakuWakuApplication.java
@@ -0,0 +1,13 @@
+package dev.backend.WakuWaku;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WakuWakuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WakuWakuApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/backend/WakuWaku/WakuWakuApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.backend.WakuWaku;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WakuWakuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
