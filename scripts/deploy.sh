1c04255c509e7791d10025c6f70f77a907f54b39
jjunsik <junsik8236@gmail.com>
2024-04-04 22:33:58 +0900

    [CD] prod 프로파일을 활성화하여 애플리케이션을 실행

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -11,14 +11,14 @@ JAR_PATH=$REPOSITORY/build/libs/$JAR_NAME
 
 CURRENT_PID=$(pgrep -f $APP_NAME)
 
-if [ -z $CURRENT_PID ]
+if [ -z "$CURRENT_PID" ];
 then
   echo "> 종료할 애플리케이션이 없습니다."
 else
   echo "> kill -9 $CURRENT_PID"
-  kill -15 $CURRENT_PID
+  kill -15 "$CURRENT_PID"
   sleep 5
 fi
 
 echo "> Deploy - $JAR_PATH "
-nohup java -jar $JAR_PATH > /dev/null 2> /dev/null < /dev/null &
+nohup java -jar -Dspring.profiles.active=prod "$JAR_PATH" > /dev/null 2> /dev/null < /dev/null &
