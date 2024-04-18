ac4d83aa44408b2966529aa02c39624b5907abc5
min2h <mhlee1304@naver.com>
2024-04-18 23:05:30 +0900

    [#22] MemberRepositoryTest 수정

--- src/test/java/dev/backend/wakuwaku/member/repository/MemberRepositoryTest.java
+++ src/test/java/dev/backend/wakuwaku/member/repository/MemberRepositoryTest.java
@@ -2,45 +2,91 @@ package dev.backend.wakuwaku.member.repository;
 
 import dev.backend.wakuwaku.domain.member.entity.MemberEntity;
 import dev.backend.wakuwaku.domain.member.repository.MemberRepository;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
+import java.util.List;
 import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
 @DataJpaTest
-public class MemberRepositoryTest {
+class MemberRepositoryTest {
 
     @Autowired
     private MemberRepository memberRepository;
 
+    @DisplayName("회원 정보를 저장합니다.")
     @Test
-    public void testFindByMemberId_ExistingMemberId() {
-        // Given
-        String memberId = "test1";
+    void saveMember() {
+        // given
+        MemberEntity member1 = createMemberEntity(1);
 
-        // When
-        Optional<MemberEntity> optionalMember = memberRepository.findByMemberId(memberId);
+        // when
+        member1 = memberRepository.save(member1);
 
-        // Then
-        assertTrue(optionalMember.isPresent()); // 회원 정보가 존재해야 함
-        MemberEntity member = optionalMember.get();
-        assertEquals(memberId, member.getMemberId()); // 회원 아이디가 일치해야 함
-        // 그 외 필요한 필드들을 확인할 수 있음
+        // then
+        MemberEntity result = memberRepository.findById(member1.getId()).orElse(null);
+        assertThat(result).isNotNull();
+        assertThat(member1).isEqualTo(result);
     }
 
-
+    @DisplayName("해당 아이디와 일치하는 회원을 조회합니다.")
     @Test
-    public void testFindByMemberId_NonExistingMemberId() {
-        // Given
-        String memberId = "NotExistingId";
+    void getMemberByMemberId() {
+        // given
+        MemberEntity member1 = createMemberEntity(1);
+        member1 = memberRepository.save(member1);
 
-        // When
-        Optional<MemberEntity> optionalMember = memberRepository.findByMemberId(memberId);
+        // when
+        Optional<MemberEntity> result = memberRepository.findByMemberId(member1.getMemberId());
 
-        // Then
-        assertFalse(optionalMember.isPresent()); // 비어 있는 Optional이 반환되어야 함
+        // then
+        assertThat(result).isPresent();
+        assertThat(member1).isEqualTo(result.get());
+    }
+
+    @DisplayName("전체 회원을 조회합니다.")
+    @Test
+    void getAllMembers() {
+        // given
+        MemberEntity member1 = createMemberEntity(1);
+        MemberEntity member2 = createMemberEntity(2);
+
+        memberRepository.save(member1);
+        memberRepository.save(member2);
+
+        // when
+        List<MemberEntity> members = memberRepository.findAll();
+
+        // then
+        assertThat(members).hasSize(2);
+    }
+
+    @DisplayName("회원을 삭제합니다.")
+    @Test
+    void deleteMember() {
+        // given
+        MemberEntity member1 = createMemberEntity(1);
+        member1 = memberRepository.save(member1);
+
+        MemberEntity member2 = createMemberEntity(2);
+        memberRepository.save(member2);
+
+        // when
+        memberRepository.deleteById(member1.getId());
+
+        // then
+        assertThat(memberRepository.findById(member1.getId())).isEmpty();
+        assertThat(memberRepository.findAll()).hasSize(1);
+    }
+
+    private MemberEntity createMemberEntity(int number) {
+        return MemberEntity.builder()
+                .memberId("testId" + number)
+                .memberEmail("testEmail" + number + "@test.com")
+                .build();
     }
 }
