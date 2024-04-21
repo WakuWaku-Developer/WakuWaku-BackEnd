a35b877f9e6618d14982a2d152680e5765795c88
min2h <mhlee1304@naver.com>
2024-04-21 15:45:25 +0900

    Test

---
+++ src/main/java/dev/backend/wakuwaku/domain/member/controller/MemberController.java
@@ -0,0 +1,15 @@
+package dev.backend.wakuwaku.domain.member.controller;
+
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("wakuwaku/v1/members")
+@RequiredArgsConstructor
+@Slf4j
+public class MemberController {
+
+}
