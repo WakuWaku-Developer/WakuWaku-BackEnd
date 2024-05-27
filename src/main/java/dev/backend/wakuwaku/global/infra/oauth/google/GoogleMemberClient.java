98ba96ca4566be900432519e4babec230a5b7d57
min2h <mhlee1304@naver.com>
2024-05-27 23:49:56 +0900

    [#41] Feat: 구글 Oauth 2.0 소셜 로그인

--- src/main/java/dev/backend/wakuwaku/global/infra/oauth/google/GoogleMemberClient.java
+++ src/main/java/dev/backend/wakuwaku/global/infra/oauth/google/GoogleMemberClient.java
@@ -6,17 +6,20 @@ import dev.backend.wakuwaku.global.infra.oauth.client.OauthMemberClient;
 import dev.backend.wakuwaku.global.infra.oauth.google.client.GoogleApiClient;
 import dev.backend.wakuwaku.global.infra.oauth.google.dto.GoogleMemberResponse;
 import dev.backend.wakuwaku.global.infra.oauth.google.dto.GoogleToken;
-import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
 
 @Component
-@RequiredArgsConstructor
 public class GoogleMemberClient implements OauthMemberClient {
 
     private final GoogleApiClient googleApiClient;
     private final GoogleOauthConfig googleOauthConfig;
+
+    public GoogleMemberClient(GoogleApiClient googleApiClient, GoogleOauthConfig googleOauthConfig) {
+        this.googleApiClient = googleApiClient;
+        this.googleOauthConfig = googleOauthConfig;
+    }
 
     @Override
     public OauthServerType supportServer() {
@@ -25,12 +28,18 @@ public class GoogleMemberClient implements OauthMemberClient {
 
     @Override
     public OauthMember fetch(String authCode) {
+        // 구글 OAuth 서버에 인증 코드를 전달하여 액세스 토큰을 요청합니다.
         GoogleToken tokenInfo = googleApiClient.fetchToken(tokenRequestParams(authCode));
-        GoogleMemberResponse googleMemberResponse = googleApiClient.fetchMember("Bearer " + tokenInfo.access_token());
+
+        // 액세스 토큰을 사용하여 사용자 정보를 요청합니다.
+        GoogleMemberResponse googleMemberResponse = googleApiClient.fetchMember(tokenInfo.access_token());
+
+        // 가져온 사용자 정보를 도메인 객체로 변환하여 반환합니다.
         return googleMemberResponse.toDomain();
     }
 
     private MultiValueMap<String, String> tokenRequestParams(String authCode) {
+        // 액세스 토큰을 요청하기 위한 매개변수를 생성합니다.
         MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
         params.add("grant_type", "authorization_code");
         params.add("client_id", googleOauthConfig.clientId());
