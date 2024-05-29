cb42504497787b35fcf819a5b0f91a2e7aaddbf6
jjunsik <junsik8236@gmail.com>
2024-05-29 17:11:54 +0900

    [#32] Feat: Restaurant Repository 구현

---
+++ src/main/java/dev/backend/wakuwaku/domain/restaurant/repository/RestaurantRepository.java
@@ -0,0 +1,12 @@
+package dev.backend.wakuwaku.domain.restaurant.repository;
+
+import dev.backend.wakuwaku.domain.restaurant.entity.Restaurant;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface RestaurantRepository extends JpaRepository<Restaurant, Long> {
+    Optional<Restaurant> findByPlaceId(String placeId);
+
+    void deleteByPlaceId(String placeId);
+}
