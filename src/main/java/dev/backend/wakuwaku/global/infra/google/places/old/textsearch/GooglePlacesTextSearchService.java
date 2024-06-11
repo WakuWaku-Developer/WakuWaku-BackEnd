c076161c13927d8bbf71d114d23ca4fe2606302d
jjunsik <junsik8236@gmail.com>
2024-06-11 22:23:07 +0900

    [#51] Refactor: GooglePlacesTextSearchService 에서 예외 발생 시 WakuWakuException 으로 예외 처리하도록 수정

--- src/main/java/dev/backend/wakuwaku/global/infra/google/places/old/textsearch/GooglePlacesTextSearchService.java
+++ src/main/java/dev/backend/wakuwaku/global/infra/google/places/old/textsearch/GooglePlacesTextSearchService.java
@@ -13,6 +13,8 @@ import org.springframework.web.client.RestClient;
 import java.util.Collections;
 import java.util.List;
 
+import static dev.backend.wakuwaku.global.exception.WakuWakuException.INVALID_SEARCH_WORD;
+import static dev.backend.wakuwaku.global.exception.WakuWakuException.NOT_EXISTED_NEXT_PAGE_TOKEN;
 import static dev.backend.wakuwaku.global.infra.google.places.old.textsearch.dto.request.TextSearchRequest.NEXT_PAGE_URL;
 import static dev.backend.wakuwaku.global.infra.google.places.old.textsearch.dto.request.TextSearchRequest.TEXT_SEARCH_URL;
 
@@ -27,6 +29,10 @@ public class GooglePlacesTextSearchService{
     private String apiKey;
 
     public List<Result> textSearch(String searchWord) {
+        if (searchWord == null || searchWord.isEmpty()) {
+            throw INVALID_SEARCH_WORD;
+        }
+
         TextSearchResponse responseByTextSearch = restClient.get()
                 .uri(textSearchURI(searchWord))
                 .retrieve()
@@ -37,6 +43,10 @@ public class GooglePlacesTextSearchService{
 
     // 나중에 사용 시 사용하는 메서드에서 이 메서드의 return 값이 null 인 경우를 처리하는 로직 구현
     public List<Result> textSearchByNextPageToken(String nextPageToken) {
+        if (nextPageToken == null || nextPageToken.isEmpty()) {
+            throw NOT_EXISTED_NEXT_PAGE_TOKEN;
+        }
+
         TextSearchResponse responseByNextPageToken = restClient.get()
                 .uri(nextPageTextSearchURI(nextPageToken))
                 .retrieve()
@@ -47,7 +57,7 @@ public class GooglePlacesTextSearchService{
 
     private List<Result> resultsByTextSearch(TextSearchResponse textSearchResponse) {
         // textSearchResponse 또는 result가 null인 경우 빈 리스트 반환
-        if (textSearchResponse == null || textSearchResponse.getResults() == null) {
+        if (textSearchResponse == null || textSearchResponse.getResults() == null || textSearchResponse.getResults().isEmpty()) {
             return Collections.emptyList();
         }
 
@@ -59,6 +69,10 @@ public class GooglePlacesTextSearchService{
             // 사진 리스트가 null이 아닌 경우에만 순회
             if (photos != null) {
                 for (PlacePhoto photo : photos) {
+                    if (photo == null || photo.getPhoto_reference() == null || photo.getPhoto_reference().isEmpty()) {
+                        continue;
+                    }
+
                     String photoUrl = getPhotoUrl(photo.getPhoto_reference());
                     photo.changeToUsablePhotoURL(photoUrl);
                 }
@@ -75,7 +89,7 @@ public class GooglePlacesTextSearchService{
     private String textSearchURI(String searchWord) {
         TextSearchRequest textSearchRequest = new TextSearchRequest(searchWord);
 
-        String newTextQuery = textSearchRequest.getTextQuery().replace(" ", "%20");
+        String newTextQuery = textSearchRequest.getTextQuery();
 
         return TEXT_SEARCH_URL + newTextQuery + "&key=" + apiKey;
     }
