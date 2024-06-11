ab846ce64e84886f55d9ed9145e06c174ab1efb8
jjunsik <junsik8236@gmail.com>
2024-06-11 22:21:29 +0900

    [#51] Feat: Exception 추가 및 WakuWakuException에 AccessLevel.PRIVATE 추가

--- src/main/java/dev/backend/wakuwaku/global/exception/ExceptionStatus.java
+++ src/main/java/dev/backend/wakuwaku/global/exception/ExceptionStatus.java
@@ -17,7 +17,14 @@ public enum ExceptionStatus {
 
     // user exception
     DUPLICATED_EMAIL(CONFLICT, 3000, "중복된 이메일이 존재합니다."),
-    NONE_USER(NOT_FOUND, 3001, "존재하지 않는 회원입니다.");
+    NONE_USER(NOT_FOUND, 3001, "존재하지 않는 회원입니다."),
+
+    // google places exception
+    INVALID_SEARCH_WORD(BAD_REQUEST, 4000, "검색어를 입력하지 않았습니다."),
+    NOT_EXISTED_NEXT_PAGE_TOKEN(BAD_REQUEST, 4001, "NEXT PAGE TOKEN 이 존재하지 않습니다."),
+    INVALID_PHOTO_REFERENCE(BAD_REQUEST, 4002, "유효하지 않은 PHOTO REFERENCE 입니다."),
+    NOT_EXISTED_PLACE_ID(BAD_REQUEST, 4003, "PLACE ID 가 존재하지 않습니다."),
+    NOT_EXISTED_DETAILS_RESPONSE(BAD_REQUEST, 4004, "Details Response 가 존재하지 않습니다.");
 
 
     private final HttpStatus httpStatus;
