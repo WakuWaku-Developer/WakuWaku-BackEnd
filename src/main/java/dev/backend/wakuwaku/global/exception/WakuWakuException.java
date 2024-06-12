7037c1214e90e1cd87437809f41909384b2b9924
jjunsik <junsik8236@gmail.com>
2024-06-13 00:27:00 +0900

    [#55] Test: 일관성을 위해 테스트 코드 변경

--- src/main/java/dev/backend/wakuwaku/global/exception/WakuWakuException.java
+++ src/main/java/dev/backend/wakuwaku/global/exception/WakuWakuException.java
@@ -9,16 +9,16 @@ import lombok.Getter;
 public class WakuWakuException extends RuntimeException {
     private final ExceptionStatus status;
 
-    public static final WakuWakuException INVALID_PARAMETER       = new WakuWakuException(ExceptionStatus.INVALID_PARAMETER);
-    public static final WakuWakuException INVALID_URL             = new WakuWakuException(ExceptionStatus.INVALID_URL);
-    public static final WakuWakuException INTERNAL_SERVER_ERROR   = new WakuWakuException(ExceptionStatus.INTERNAL_SERVER_ERROR);
-    public static final WakuWakuException NOT_EXISTED_FILE        = new WakuWakuException(ExceptionStatus.NOT_EXISTED_FILE);
-    public static final WakuWakuException DUPLICATED_EMAIL        = new WakuWakuException(ExceptionStatus.DUPLICATED_EMAIL);
-    public static final WakuWakuException NONE_USER               = new WakuWakuException(ExceptionStatus.NONE_USER);
-    public static final WakuWakuException INVALID_SEARCH_WORD               = new WakuWakuException(ExceptionStatus.INVALID_SEARCH_WORD);
-    public static final WakuWakuException NOT_EXISTED_NEXT_PAGE_TOKEN               = new WakuWakuException(ExceptionStatus.NOT_EXISTED_NEXT_PAGE_TOKEN);
-    public static final WakuWakuException INVALID_PHOTO_REFERENCE               = new WakuWakuException(ExceptionStatus.INVALID_PHOTO_REFERENCE);
-    public static final WakuWakuException NOT_EXISTED_PLACE_ID = new WakuWakuException(ExceptionStatus.NOT_EXISTED_PLACE_ID);
-    public static final WakuWakuException NOT_EXISTED_DETAILS_RESPONSE = new WakuWakuException(ExceptionStatus.NOT_EXISTED_DETAILS_RESPONSE);
+    public static final WakuWakuException INVALID_PARAMETER             = new WakuWakuException(ExceptionStatus.INVALID_PARAMETER);
+    public static final WakuWakuException INVALID_URL                   = new WakuWakuException(ExceptionStatus.INVALID_URL);
+    public static final WakuWakuException INTERNAL_SERVER_ERROR         = new WakuWakuException(ExceptionStatus.INTERNAL_SERVER_ERROR);
+    public static final WakuWakuException NOT_EXISTED_FILE              = new WakuWakuException(ExceptionStatus.NOT_EXISTED_FILE);
+    public static final WakuWakuException DUPLICATED_EMAIL              = new WakuWakuException(ExceptionStatus.DUPLICATED_EMAIL);
+    public static final WakuWakuException NONE_USER                     = new WakuWakuException(ExceptionStatus.NONE_USER);
+    public static final WakuWakuException INVALID_SEARCH_WORD           = new WakuWakuException(ExceptionStatus.INVALID_SEARCH_WORD);
+    public static final WakuWakuException NOT_EXISTED_NEXT_PAGE_TOKEN   = new WakuWakuException(ExceptionStatus.NOT_EXISTED_NEXT_PAGE_TOKEN);
+    public static final WakuWakuException INVALID_PHOTO_REFERENCE       = new WakuWakuException(ExceptionStatus.INVALID_PHOTO_REFERENCE);
+    public static final WakuWakuException NOT_EXISTED_PLACE_ID          = new WakuWakuException(ExceptionStatus.NOT_EXISTED_PLACE_ID);
+    public static final WakuWakuException NOT_EXISTED_DETAILS_RESPONSE  = new WakuWakuException(ExceptionStatus.NOT_EXISTED_DETAILS_RESPONSE);
 
 }
