04a89ca40e96d3b6307fad8f12cc2a3e25243b82
jjunsik <junsik8236@gmail.com>
2024-06-13 00:39:10 +0900

    [#55] Remove: 응답 데이터에 성공 여부 필드 삭제

--- src/main/java/dev/backend/wakuwaku/global/response/BaseResponse.java
+++ src/main/java/dev/backend/wakuwaku/global/response/BaseResponse.java
@@ -6,7 +6,6 @@ import lombok.Getter;
 
 @Getter
 public class BaseResponse<T> {
-    private final Boolean isSuccess;
     private final int code;
     private final String message;
 
@@ -15,7 +14,6 @@ public class BaseResponse<T> {
 
     // 성공 시 반환할 데이터가 있을 때 반환되는 Response
     public BaseResponse(T data) {
-        this.isSuccess = true;
         this.code = 1000;
         this.message = "요청에 성공하였습니다.";
         this.data = data;
@@ -23,14 +21,12 @@ public class BaseResponse<T> {
 
     // 성공 시 반환할 데이터가 없을 때 반환되는 Response
     public BaseResponse() {
-        this.isSuccess = true;
         this.code = 1000;
         this.message = "요청에 성공하였습니다.";
     }
 
     // 실패 시 반환되는 Response
     public BaseResponse(ExceptionStatus status) {
-        this.isSuccess = false;
         this.code = status.getCode();
         this.message = status.getMessage();
     }
