c8074cfb7e69cc8a29b59a6fee3cabc9e67ca3b4
min2h <mhlee1304@naver.com>
2024-06-23 23:57:05 +0900

    [#41] Fix: ExceptionStatus 예외 코드 추가

--- src/main/java/dev/backend/wakuwaku/global/exception/ExceptionStatus.java
+++ src/main/java/dev/backend/wakuwaku/global/exception/ExceptionStatus.java
@@ -19,6 +19,13 @@ public enum ExceptionStatus {
     DUPLICATED_EMAIL(CONFLICT, 3000, "중복된 이메일이 존재합니다."),
     NONE_USER(NOT_FOUND, 3001, "존재하지 않는 회원입니다."),
 
+    // oauth exception
+    FALIED_TO_LOGIN(NOT_FOUND ,3002, "로그인 중 오류가 발생했습니다. 다시 시도해 주세요."),
+    NOT_EXISTED_SOCIAL_TYPE(NOT_FOUND ,3003, "지원하지 않는 소셜 타입입니다."),
+    NOT_EXISTED_MEMBER_INFO(NOT_FOUND ,3004, "사용자 정보를 찾을 수 없습니다."),
+
+
+
     // google places exception
     INVALID_SEARCH_WORD(BAD_REQUEST, 4000, "검색어를 입력하지 않았습니다."),
     NOT_EXISTED_NEXT_PAGE_TOKEN(BAD_REQUEST, 4001, "NEXT PAGE TOKEN 이 존재하지 않습니다."),
