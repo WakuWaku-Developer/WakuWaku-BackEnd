58a1a8473035207715866b6bb9efb5e4f00b41bb
jjunsik <junsik8236@gmail.com>
2024-07-09 15:41:05 +0900

    [#63] Refactor: Wrapper 클래스를 활용하여 응답하도록 Controller 수정

--- src/main/java/dev/backend/wakuwaku/domain/restaurant/controller/RestaurantController.java
+++ src/main/java/dev/backend/wakuwaku/domain/restaurant/controller/RestaurantController.java
@@ -1,8 +1,8 @@
 package dev.backend.wakuwaku.domain.restaurant.controller;
 
 import dev.backend.wakuwaku.domain.restaurant.dto.response.DetailsInfoRestaurantResponse;
+import dev.backend.wakuwaku.domain.restaurant.dto.response.Restaurants;
 import dev.backend.wakuwaku.domain.restaurant.dto.response.SimpleInfoRestaurantResponse;
-import dev.backend.wakuwaku.domain.restaurant.entity.Restaurant;
 import dev.backend.wakuwaku.domain.restaurant.service.RestaurantService;
 import dev.backend.wakuwaku.global.infra.google.places.dto.Places;
 import dev.backend.wakuwaku.global.response.BaseResponse;
@@ -20,9 +20,9 @@ public class RestaurantController {
     @GetMapping
     public BaseResponse<List<SimpleInfoRestaurantResponse>> getSimpleInfoRestaurants(@RequestParam("search") String searchWord) {
 
-        List<Restaurant> restaurants = restaurantService.getSimpleRestaurants(searchWord);
+        Restaurants restaurants = restaurantService.getSimpleRestaurants(searchWord);
 
-        return new BaseResponse<>(restaurants.stream()
+        return new BaseResponse<>(restaurants.getRestaurants().stream()
                 .map(SimpleInfoRestaurantResponse::new)
                 .toList());
     }
