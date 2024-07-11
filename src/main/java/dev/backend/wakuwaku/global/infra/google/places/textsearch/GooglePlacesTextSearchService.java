866e2c0e40e048aafa28eb15fc1d04ae3b94677d
jjunsik <junsik8236@gmail.com>
2024-07-11 22:57:16 +0900

    [#63] Fix: Google Places API 호출 시 Next Page Token 을 활용한 요청까지 완료한 데이터를 응답하도록 수정

--- src/main/java/dev/backend/wakuwaku/global/infra/google/places/textsearch/GooglePlacesTextSearchService.java
+++ src/main/java/dev/backend/wakuwaku/global/infra/google/places/textsearch/GooglePlacesTextSearchService.java
@@ -50,12 +50,12 @@ public class GooglePlacesTextSearchService {
 
     // pageToken 을 얻은 searchWord 와 같은 searchWord 로 검색해야만 제대로 동작함. (그렇지 않으면 에러 발생)
     // pageToken 만 가지고는 API 요청할 수 없음. (에러 발생)
-    public List<Places> getRestaurantByNextPageToken(String searchWord, String pageToken, int cnt) {
+    public TextSearchResponse getRestaurantByNextPageToken(String searchWord, String pageToken) {
         if (pageToken == null || pageToken.isEmpty()) {
-            return Collections.emptyList();
+            return null;
         }
 
-        TextSearchResponse textSearchResponse = restClient.post()
+        return restClient.post()
                 .uri(TEXT_SEARCH_URL)
                 .header(TEXT_SEARCH_RESPONSE_FIELDS_HEADER, TEXT_SEARCH_RESPONSE_FIELDS)
                 .header(TEXT_SEARCH_REQUEST_API_KEY_HEADER, apiKey)
@@ -63,8 +63,6 @@ public class GooglePlacesTextSearchService {
                 .body(getNextPageRequestBody(searchWord, pageToken))
                 .retrieve()
                 .body(TextSearchResponse.class);
-
-        return getPlaces(textSearchResponse, searchWord, cnt);
     }
 
     private List<Places> getPlaces(TextSearchResponse textSearchResponse, String searchWord, int cnt) {
@@ -72,10 +70,6 @@ public class GooglePlacesTextSearchService {
             return Collections.emptyList();
         }
 
-        return getUsablePlaces(textSearchResponse, searchWord, cnt);
-    }
-
-    private List<Places> getUsablePlaces(TextSearchResponse textSearchResponse, String searchWord, int cnt) {
         List<Places> places = textSearchResponse.getPlaces();
 
         List<Places> actualPlaces = new ArrayList<>();
@@ -104,6 +98,71 @@ public class GooglePlacesTextSearchService {
 
         if (isResultEnough(actualPlaces) && cnt <= 3) {
             return getRestaurantsByTextSearch(searchWord, cnt);
+        }
+
+        return getPlacesAllByNextPageToken(textSearchResponse, searchWord, actualPlaces);
+    }
+
+    // next page token 과 결과값이 존재할 때까지 검색
+    private List<Places> getPlacesAllByNextPageToken(TextSearchResponse textSearchResponse, String searchWord, List<Places> actualPlaces) {
+        TextSearchResponse textSearchResponseByNextPageToken = getRestaurantByNextPageToken(searchWord, textSearchResponse.getNextPageToken());
+
+        if (textSearchResponseByNextPageToken == null) {
+            return actualPlaces;
+        }
+
+        List<Places> placesByNextPageToken = getPlacesByNextPageToken(textSearchResponseByNextPageToken);
+
+        if (!placesByNextPageToken.isEmpty()) {
+            actualPlaces.addAll(placesByNextPageToken);
+        }
+
+        String nextPageToken = textSearchResponseByNextPageToken.getNextPageToken();
+
+        while (nextPageToken != null && !nextPageToken.isBlank() && !placesByNextPageToken.isEmpty()) {
+            TextSearchResponse retryTextSearchResponseByNextPageToken = getRestaurantByNextPageToken(searchWord, nextPageToken);
+
+            placesByNextPageToken = getPlacesByNextPageToken(retryTextSearchResponseByNextPageToken);
+
+            if (!placesByNextPageToken.isEmpty()) {
+                actualPlaces.addAll(placesByNextPageToken);
+            }
+
+            nextPageToken = retryTextSearchResponseByNextPageToken.getNextPageToken();
+        }
+
+        return actualPlaces;
+    }
+
+    private List<Places> getPlacesByNextPageToken(TextSearchResponse textSearchResponse) {
+        if (textSearchResponse == null || textSearchResponse.getPlaces() == null || textSearchResponse.getPlaces().isEmpty()) {
+            return Collections.emptyList();
+        }
+
+        List<Places> places = textSearchResponse.getPlaces();
+
+        List<Places> actualPlaces = new ArrayList<>();
+
+        for (Places place : places) {
+            List<Photo> photos = place.getPhotos();
+
+            if (photos == null || photos.isEmpty() || place.getPlusCode() == null || place.getPlusCode().getCompoundCode() == null || place.getPlusCode().getCompoundCode().contains(INVALID_RESTAURANT_LOCATION)) {
+                continue;
+            }
+
+            // 하나의 사진(대표 사진)만을 얻으면 되므로
+            // 각 식당 객체 사진에 대한 API 를 한 번씩만 호출
+            Photo photo = photos.get(0);
+
+            String actualPhotoUrl = googlePlacesPhotoService.getActualPhotoUrl(photo, apiKey);
+            photo.createPhotoURL(actualPhotoUrl);
+
+            // 첫 번째 값 제외하고 나머지 9개의 값이 null 로 표시되므로
+            // size() 가 1인 리스트를 반환
+            photos.clear();
+            photos.add(photo);
+
+            actualPlaces.add(place);
         }
 
         return actualPlaces;
