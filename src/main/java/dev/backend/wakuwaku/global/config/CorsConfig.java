82c673b8cd7dae94f4c7eb31a84a5ab65ea738d6
min2h <mhlee1304@naver.com>
2024-07-11 22:11:52 +0900

    [#69] Fix: CorsConfig, WebConfig 병합 [allowCredentials(true) > 인증 정보가 필요할 때 사용하는 설정 유지]

--- src/main/java/dev/backend/wakuwaku/global/config/CorsConfig.java
+++ src/main/java/dev/backend/wakuwaku/global/config/CorsConfig.java
@@ -10,6 +10,7 @@ public class CorsConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins("http://localhost:3000", "https://wakuwaku-jp.vercel.app/")
-                .allowedMethods("*");
+                .allowCredentials(true)
+                .allowedMethods("GET", "POST", "PUT", "DELETE");
     }
 }
