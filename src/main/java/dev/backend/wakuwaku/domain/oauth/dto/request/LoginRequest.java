09e819975669be11e5098ad901a5e3e54da01386
min2h <mhlee1304@naver.com>
2024-07-28 17:02:53 +0900

    [#74] Fix: DTO - LoginRequest 추가

--- src/main/java/dev/backend/wakuwaku/domain/oauth/dto/request/LoginRequest.java
+++ src/main/java/dev/backend/wakuwaku/domain/oauth/dto/request/LoginRequest.java
@@ -12,8 +12,6 @@ public class LoginRequest {
     private OauthServerType oauthServerType;
     private String code;
 
-    // Getters and setters
-
     public OauthServerType getOauthServerType() {
         return oauthServerType;
     }
