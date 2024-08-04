4a500a22cc6117384b91066bf1818ed8c9af707a
min2h <mhlee1304@naver.com>
2024-08-04 17:15:42 +0900

    [#74] Feat: Favorite Entity 추가

--- src/main/java/dev/backend/wakuwaku/domain/favorite/entity/Favorite.java
+++ src/main/java/dev/backend/wakuwaku/domain/favorite/entity/Favorite.java
@@ -1,4 +1,29 @@
 package dev.backend.wakuwaku.domain.favorite.entity;
 
-public class Favorite {
+import dev.backend.wakuwaku.domain.BaseEntity;
+import dev.backend.wakuwaku.domain.member.entity.Member;
+import dev.backend.wakuwaku.domain.restaurant.entity.Restaurant;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+
+@Entity
+@Setter
+@Getter
+@Table(name = "favorites_table")
+public class Favorite extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long favoritesId;
+
+    @ManyToOne
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    //@JoinColumn
+
+    @ManyToOne
+    @JoinColumn(name = "restaurant_id")
+    private Restaurant restaurant;
 }
