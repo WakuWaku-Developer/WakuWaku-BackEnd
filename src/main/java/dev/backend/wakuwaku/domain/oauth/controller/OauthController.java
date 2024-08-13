0c96a1e321d8ec01101bd5460d63b61300b24038
min2h <mhlee1304@naver.com>
2024-08-13 22:45:10 +0900

    [#80] Fix: Oauth 서비스, 컨트롤러 불필요한 메서드 삭제

--- src/main/java/dev/backend/wakuwaku/domain/oauth/controller/OauthController.java
+++ src/main/java/dev/backend/wakuwaku/domain/oauth/controller/OauthController.java
@@ -1,14 +1,14 @@
 package dev.backend.wakuwaku.domain.oauth.controller;
 
-import dev.backend.wakuwaku.domain.oauth.dto.OauthServerType;
 import dev.backend.wakuwaku.domain.oauth.dto.request.LoginRequest;
 import dev.backend.wakuwaku.domain.oauth.service.OauthService;
 import dev.backend.wakuwaku.global.response.BaseResponse;
-import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-import java.io.IOException;
 import java.util.Map;
 
 @RestController
@@ -17,31 +17,6 @@ import java.util.Map;
 public class OauthController {
 
     private final OauthService oauthService;
-
-    @GetMapping("/{oauthServerType}")
-    BaseResponse<Void> redirectAuthCodeRequestUrl(
-            @PathVariable(name = "oauthServerType") String oauthServerTypeStr,
-            HttpServletResponse response
-    ) throws IOException {
-
-        OauthServerType oauthServerType = OauthServerType.fromName(oauthServerTypeStr);
-        if (oauthServerType == null) {
-            return new BaseResponse<>();
-        }
-
-        String redirectUrl = oauthService.getAuthCodeRequestUrl(oauthServerType);
-        if ("google".equals(oauthServerTypeStr)) {
-            // 스코프 파라미터를 추가하기 전에 이전에 포함된 스코프 파라미터를 제거
-            redirectUrl = redirectUrl.replaceAll("&scope=[^&]*", "");
-            redirectUrl += "&scope=profile%20email%20profile%20openid&access_type=offline";
-            // 스페이스를 URL 인코딩하여 추가
-        }
-
-        response.sendRedirect(redirectUrl);
-
-        return new BaseResponse<>();
-    }
-
 
     @PostMapping("/login")
     BaseResponse<Map<String, Long>> login(@RequestBody LoginRequest loginRequest) {
