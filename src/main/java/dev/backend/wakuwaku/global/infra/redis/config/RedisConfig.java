1a709e8a1188574a5fae0c70d405947a2e50efa3
jjunsik <junsik8236@gmail.com>
2024-08-14 16:15:01 +0900

    Remove: RedisConfig 에서 불필요한 메서드와 어노테이션 삭제

--- src/main/java/dev/backend/wakuwaku/global/infra/redis/config/RedisConfig.java
+++ src/main/java/dev/backend/wakuwaku/global/infra/redis/config/RedisConfig.java
@@ -1,21 +1,14 @@
 package dev.backend.wakuwaku.global.infra.redis.config;
 
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.cache.CacheManager;
-import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.redis.cache.RedisCacheConfiguration;
-import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
-import org.springframework.data.redis.serializer.RedisSerializationContext;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
-
-import java.time.Duration;
 
 /**
  * yml 파일에 정의한 내용을 기반으로 Redis 클라이언트를 구성하기 위한 Configuration 클래스
@@ -24,7 +17,6 @@ import java.time.Duration;
  * 때문에, @EnableCaching 어노테이션을 설정 클래스에 추가해주어야 한다
  */
 @Configuration
-@EnableCaching
 public class RedisConfig {
     @Value("${spring.data.redis.host}")
     private String redisHost;
@@ -46,28 +38,6 @@ public class RedisConfig {
         return new LettuceConnectionFactory(redisStandaloneConfiguration);
     }
 
-    /**
-     * 캐시 만료 시간을 3일로 설정
-     * <p>enableTimeToIdle() 을 추가하여 캐시 히트 시 캐시 만료 시간(TTL) 갱신
-     * <p>1. 캐시를 관리하고 캐시 된 ‘데이터를 저장’하고 ‘반환’하는 역할
-     * <p>2. 일반적으로 메모리 상에 저장되는 데이터를 디스크, 데이터베이스, 클라우드 서비스와 같은 저장소에 저장이 가능하도록 함
-     * <p>3. 해당 캐시는 Redis, Ehcache, Caffeine 등 다양한 캐시 라이브러리와 연동하여 사용할 수 있음
-     */
-    @Bean
-    public CacheManager redisCacheManager(RedisConnectionFactory redisConnectionFactory){
-
-        RedisCacheConfiguration conf = RedisCacheConfiguration.defaultCacheConfig()
-                .serializeKeysWith(RedisSerializationContext.SerializationPair.fromSerializer(new StringRedisSerializer()))
-                .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(new GenericJackson2JsonRedisSerializer()))
-                .entryTtl(Duration.ofDays(3))
-                .enableTimeToIdle();
-
-        return RedisCacheManager.RedisCacheManagerBuilder
-                .fromConnectionFactory(redisConnectionFactory)
-                .cacheDefaults(conf)
-                .build();
-    }
-
     @Bean
     public RedisTemplate<String, Object> redisTemplate() {
         RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
@@ -76,13 +46,6 @@ public class RedisConfig {
         // 일반적인 key:value의 경우 시리얼라이저
         redisTemplate.setKeySerializer(new StringRedisSerializer());
         redisTemplate.setValueSerializer(new GenericJackson2JsonRedisSerializer());
-
-//        // Hash를 사용할 경우 시리얼라이저
-//        redisTemplate.setHashKeySerializer(new StringRedisSerializer());
-//        redisTemplate.setHashValueSerializer(new StringRedisSerializer());
-//
-//        // 모든 경우
-//        redisTemplate.setDefaultSerializer(new StringRedisSerializer());
 
         return redisTemplate;
     }
