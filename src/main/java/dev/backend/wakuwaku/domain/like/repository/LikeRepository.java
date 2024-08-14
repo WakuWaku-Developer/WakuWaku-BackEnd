39a1fd463e1c12779e70749d7c4a0d8eb6cb04ec
min2h <mhlee1304@naver.com>
2024-08-14 22:50:50 +0900

    [#82] Feat: Like Repository 추가

---
+++ src/main/java/dev/backend/wakuwaku/domain/like/repository/LikeRepository.java
@@ -0,0 +1,22 @@
+package dev.backend.wakuwaku.domain.like.repository;
+
+import dev.backend.wakuwaku.domain.like.entity.Like;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+
+@Repository
+public interface LikeRepository extends JpaRepository<Like, Long> {
+
+    @Query("SELECT l FROM Like l WHERE l.member.id = :memberId AND l.restaurant.placeId = :restaurantId")
+    Optional<Like> findByMemberIdAndRestaurantId(@Param("memberId") Long memberId, @Param("restaurantId") String restaurantId);
+
+    // 찜 리스트
+    @Query("SELECT l FROM Like l WHERE l.member.id = :memberId AND l.likeStatus = 'Y'")
+    List<Like> findLikeStatusAllByMemberId(@Param("memberId") Long memberId);
+}
