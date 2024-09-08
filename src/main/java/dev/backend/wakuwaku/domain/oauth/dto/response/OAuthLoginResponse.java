9a0bad3d0b1adc76ced6a77275372203b159f234
jjunsik <junsik8236@gmail.com>
2024-09-08 20:32:39 +0900

    [#78] Feat: 로그인 요청 후 반환 값에 해당 회원의 찜 식당 PlaceId를 추가하여 반환하도록 수정

---
+++ src/main/java/dev/backend/wakuwaku/domain/oauth/dto/response/OAuthLoginResponse.java
@@ -0,0 +1,21 @@
+package dev.backend.wakuwaku.domain.oauth.dto.response;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@NoArgsConstructor
+public class OAuthLoginResponse {
+    private Long id;
+    private List<String> likedRestaurantPlaceIds = new ArrayList<>();
+
+    @Builder
+    public OAuthLoginResponse(Long id, List<String> likedRestaurantPlaceIds) {
+        this.id = id;
+        this.likedRestaurantPlaceIds = likedRestaurantPlaceIds;
+    }
+}
