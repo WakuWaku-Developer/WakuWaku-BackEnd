d135437ddc48061d4d631d973d0790efb5e081b9
jjunsik <junsik8236@gmail.com>
2024-09-08 19:54:44 +0900

    [#78] Feat: 로그인 시 해당 회원의 찜 식당의 PlaceId를 같이 반환하도록 기능 수정

--- src/main/java/dev/backend/wakuwaku/domain/oauth/controller/OauthController.java
+++ src/main/java/dev/backend/wakuwaku/domain/oauth/controller/OauthController.java
@@ -1,6 +1,9 @@
 package dev.backend.wakuwaku.domain.oauth.controller;
 
+import dev.backend.wakuwaku.domain.likes.service.LikesService;
+import dev.backend.wakuwaku.domain.member.entity.Member;
 import dev.backend.wakuwaku.domain.oauth.dto.request.LoginRequest;
+import dev.backend.wakuwaku.domain.oauth.dto.response.OAuthLoginResponse;
 import dev.backend.wakuwaku.domain.oauth.service.OauthService;
 import dev.backend.wakuwaku.global.response.BaseResponse;
 import lombok.RequiredArgsConstructor;
@@ -9,20 +12,24 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Map;
+import java.util.List;
 
 @RestController
 @RequestMapping("/oauth")
 @RequiredArgsConstructor
 public class OauthController {
-
     private final OauthService oauthService;
 
-    @PostMapping("/login")
-    BaseResponse<Map<String, Long>> login(@RequestBody LoginRequest loginRequest) {
+    private final LikesService likesService;
 
-        Map<String, Long> login = oauthService.login(loginRequest.getOauthServerType(), loginRequest.getCode());
-        return new BaseResponse<>(login);
+    @PostMapping("/login")
+    BaseResponse<OAuthLoginResponse> login(@RequestBody LoginRequest loginRequest) {
+        Member member = oauthService.login(loginRequest.getOauthServerType(), loginRequest.getCode());
+        List<String> placeIdOfLikesRestaurants = likesService.getLikedRestaurantPlaceIds(member);
+
+        return new BaseResponse<>(OAuthLoginResponse.builder()
+                .id(member.getId())
+                .likedRestaurantPlaceIds(placeIdOfLikesRestaurants)
+                .build());
     }
 }
-
