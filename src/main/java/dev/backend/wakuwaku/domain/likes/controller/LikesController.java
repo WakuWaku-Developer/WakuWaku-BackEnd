aefbde057232445ccf2a526d9f42755e168646f5
jjunsik <junsik8236@gmail.com>
2024-09-09 23:56:21 +0900

    [#86] Feat: 회원의 찜 목록을 페이지 별로 조회하는 API 개발

--- src/main/java/dev/backend/wakuwaku/domain/likes/controller/LikesController.java
+++ src/main/java/dev/backend/wakuwaku/domain/likes/controller/LikesController.java
@@ -6,15 +6,22 @@ import dev.backend.wakuwaku.domain.likes.entity.Likes;
 import dev.backend.wakuwaku.domain.likes.service.LikesService;
 import dev.backend.wakuwaku.global.response.BaseResponse;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/wakuwaku/v1/likes")
 @RequiredArgsConstructor
-@Slf4j
 public class LikesController {
     private final LikesService likesService;
+
+    @GetMapping("/{memberId}")
+    public BaseResponse<AllLikesResponse> getAllLikes(@PathVariable("memberId") Long memberId, @PageableDefault(size = 10) Pageable pageable) {
+        AllLikesResponse paginatedLikes = likesService.getPaginatedLikesList(memberId, pageable);
+
+        return new BaseResponse<>(paginatedLikes);
+    }
 
     @PostMapping("/push")
     public BaseResponse<LikesResponse> pushLikes(@RequestBody LikesRequest likesRequest) {
