a2310f2a5d7b43fdd021390ccbc000d115322b41
jjunsik <junsik8236@gmail.com>
2024-09-10 00:00:44 +0900

    [#86] Feat: PlaceId 리스트로 식당 조회하는 Repository 구현

--- src/main/java/dev/backend/wakuwaku/domain/restaurant/repository/RestaurantRepository.java
+++ src/main/java/dev/backend/wakuwaku/domain/restaurant/repository/RestaurantRepository.java
@@ -2,9 +2,17 @@ package dev.backend.wakuwaku.domain.restaurant.repository;
 
 import dev.backend.wakuwaku.domain.restaurant.entity.Restaurant;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface RestaurantRepository extends JpaRepository<Restaurant, Long> {
     Optional<Restaurant> findByPlaceId(String placeId);
+
+    @Query("select r " +
+            "from Restaurant r " +
+            "where r.placeId in :placeIds")
+    List<Restaurant> findAllByPlaceIds(@Param("placeIds") List<String> placeIds);
 }
