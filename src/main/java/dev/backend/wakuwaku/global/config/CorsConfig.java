fd9f8efa716937b476727e601ad5dab46ba64d1b
jjunsik <junsik8236@gmail.com>
2024-09-15 22:25:25 +0900

    [#98] Fix: 구매한 도메인 CORS 추가

--- src/main/java/dev/backend/wakuwaku/global/config/CorsConfig.java
+++ src/main/java/dev/backend/wakuwaku/global/config/CorsConfig.java
@@ -9,7 +9,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000", "https://wakuwaku-jp.vercel.app/")
+                .allowedOrigins("http://localhost:3000", "https://wakuwaku-place.store")
                 .allowCredentials(true)
                 .allowedMethods("GET", "POST", "PUT", "DELETE");
     }
