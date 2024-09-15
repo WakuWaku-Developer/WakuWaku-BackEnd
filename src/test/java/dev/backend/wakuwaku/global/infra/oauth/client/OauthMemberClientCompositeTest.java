7451d4c64613918e3e85e9a25fc10d2cea7e1814
jjunsik <junsik8236@gmail.com>
2024-09-16 02:58:03 +0900

    [#97] Test: 구글 계정으로 로그인 시, 사용자 닉네임 가져오는 로직으로 인한 테스트 코드 추가 및 수정

--- src/test/java/dev/backend/wakuwaku/global/infra/oauth/client/OauthMemberClientCompositeTest.java
+++ src/test/java/dev/backend/wakuwaku/global/infra/oauth/client/OauthMemberClientCompositeTest.java
@@ -78,7 +78,51 @@ class OauthMemberClientCompositeTest {
         // Then
         assertEquals("12345", oauthMember.getOauthId().getOauthServerId());
         assertEquals("test@gmail.com", oauthMember.getEmail());
-        assertEquals("John Doe", oauthMember.getNickname());
+        assertEquals("DoeJohn", oauthMember.getNickname());
+        assertEquals("https://example.com/picture", oauthMember.getProfileImageUrl());
+    }
+
+    @Test
+    @DisplayName("Oauth Login Type에 관련된 사용자 정보 가져오기(성이 null인 사용자)")
+    void testFetch_No_Last_Name() {
+        // Given
+        String mockAuthCode = "mockAuthCode";
+
+        OauthServerType oauthServerType = OauthServerType.GOOGLE;
+
+        GoogleToken mockToken = new GoogleToken("mockAccessToken", "bearer", 3600, null, "mockIdToken");
+
+        GoogleMemberResponse mockResponse = new GoogleMemberResponse("12345", "test@gmail.com", "John", null, "https://example.com/picture");
+
+        // Mock GoogleOauthConfig에서 필요한 값들 설정
+        when(mockGoogleOauthConfig.getClientId()).thenReturn("mockClientId");
+        when(mockGoogleOauthConfig.getClientSecret()).thenReturn("mockClientSecret");
+        when(mockGoogleOauthConfig.getRedirectUri()).thenReturn("mockRedirectUri");
+        when(mockGoogleOauthConfig.getTokenUri()).thenReturn("mockTokenUri");
+        when(mockGoogleOauthConfig.getResourceUri()).thenReturn("mockResourceUri");
+
+        // MultiValueMap 생성
+        MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
+
+        params.add("grant_type", "authorization_code");
+        params.add("client_id", mockGoogleOauthConfig.getClientId());
+        params.add("client_secret", mockGoogleOauthConfig.getClientSecret());
+        params.add("code", mockAuthCode);
+        params.add("redirect_uri", mockGoogleOauthConfig.getRedirectUri());
+        params.add("token_uri", mockGoogleOauthConfig.getTokenUri());
+        params.add("resource_uri", mockGoogleOauthConfig.getResourceUri());
+
+        // Mockito를 사용하여 fetchToken 및 fetchMember 메서드의 반환값 설정
+        when(mockGoogleApiClient.fetchToken(params)).thenReturn(mockToken);
+        when(mockGoogleApiClient.fetchMember(mockToken.access_token())).thenReturn(mockResponse);
+
+        // When
+        OauthMember oauthMember = clientComposite.fetch(oauthServerType, mockAuthCode);
+
+        // Then
+        assertEquals("12345", oauthMember.getOauthId().getOauthServerId());
+        assertEquals("test@gmail.com", oauthMember.getEmail());
+        assertEquals("John", oauthMember.getNickname());
         assertEquals("https://example.com/picture", oauthMember.getProfileImageUrl());
     }
 }
