57ad8ad8fe656dab40b386de34eaf9796c54bda5
jjunsik <junsik8236@gmail.com>
2024-09-17 21:00:53 +0900

    [#107] Fix: KakaoApiClient 에서 @RequestParam 을 @RequestBody 로 수정

--- src/main/java/dev/backend/wakuwaku/global/infra/oauth/kakao/client/KakaoApiClient.java
+++ src/main/java/dev/backend/wakuwaku/global/infra/oauth/kakao/client/KakaoApiClient.java
@@ -3,8 +3,8 @@ package dev.backend.wakuwaku.global.infra.oauth.kakao.client;
 import dev.backend.wakuwaku.global.infra.oauth.kakao.dto.KakaoMemberResponse;
 import dev.backend.wakuwaku.global.infra.oauth.kakao.dto.KakaoToken;
 import org.springframework.util.MultiValueMap;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.service.annotation.GetExchange;
 import org.springframework.web.service.annotation.PostExchange;
 
@@ -14,7 +14,7 @@ import static org.springframework.http.MediaType.APPLICATION_FORM_URLENCODED_VAL
 
 public interface KakaoApiClient {
     @PostExchange(url = "https://kauth.kakao.com/oauth/token", contentType = APPLICATION_FORM_URLENCODED_VALUE)
-    KakaoToken fetchToken(@RequestParam("MultiValueMap") MultiValueMap<String, String> params);
+    KakaoToken fetchToken(@RequestBody MultiValueMap<String, String> params);
 
     // 추가
     @GetExchange(url = "https://kapi.kakao.com/v2/user/me")
