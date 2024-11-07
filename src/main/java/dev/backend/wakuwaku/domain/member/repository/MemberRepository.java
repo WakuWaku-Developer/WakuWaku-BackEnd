bf4899c47ae3d67a37f864e08d5b380c136b40c9
jjunsik <junsik8236@gmail.com>
2024-11-07 19:56:14 +0900

    [#114] Feat: ACTIVE 상태인 Member만 조회하는 전체 조회 쿼리 구현

--- src/main/java/dev/backend/wakuwaku/domain/member/repository/MemberRepository.java
+++ src/main/java/dev/backend/wakuwaku/domain/member/repository/MemberRepository.java
@@ -2,9 +2,16 @@ package dev.backend.wakuwaku.domain.member.repository;
 
 import dev.backend.wakuwaku.domain.member.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
     Optional<Member> findByEmail(String email);
+
+    @Query("select m " +
+            "from Member m " +
+            "where m.status = 'ACTIVE'")
+    List<Member> findAllByActiveMember();
 }
